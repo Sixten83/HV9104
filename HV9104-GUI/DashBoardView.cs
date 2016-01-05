9f3f4b5d30ece7d171953b404acbbf0fb73cd744
Sixten83 <sixten.isaksson@terco.se>
2016-01-05 14:32:18 +0100

    dashboardview2

---
+++ HV9104-GUI/DashBoardView.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HV9104_GUI
+{
+    public partial class DashBoardView : UserControl
+    {
+
+        public DashBoardView()
+        {
+            InitializeComponent();
+        }
+        //protected override CreateParams CreateParams
+        //{
+        //    get
+        //    {
+        //        CreateParams cp = base.CreateParams;
+        //        cp.ExStyle = cp.ExStyle | 0x2000000;
+        //        return cp;
+        //    }
+        //}
+ 
+
+        //private void RunView_Load(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void customPanel5_Paint(object sender, PaintEventArgs e)
+        //{
+
+        //}
+
+        //private void customPanel11_Paint(object sender, PaintEventArgs e)
+        //{
+
+        //}
+
+        //private void trafSpeedTrackBar_valueChangeHandler(object sender, ValueChangeEventArgs e)
+        //{
+
+        //}
+
+
+    }
+}
